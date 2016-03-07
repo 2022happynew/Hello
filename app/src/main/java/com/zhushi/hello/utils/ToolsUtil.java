29306ccb740ec72592f5b3eb67e202ee08f846b7
zhushi <zhushi@baidu.com>
2016-03-07 14:52:38 +0800

    天气预报数据获取和UI实现

--- app/src/main/java/com/zhushi/hello/utils/ToolsUtil.java
+++ app/src/main/java/com/zhushi/hello/utils/ToolsUtil.java
@@ -1,6 +1,8 @@
 package com.zhushi.hello.utils;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 /**
  * Created by zhushi on 2016/3/4.
@@ -40,5 +42,29 @@ public class ToolsUtil {
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 判断网络是否可用
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            //如果仅仅是用来判断网络连接
+            //则可以使用 cm.getActiveNetworkInfo().isAvailable();
+            NetworkInfo[] info = cm.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/com/zhushi/hello/weather/model/WeatherModel.java
+++ app/src/main/java/com/zhushi/hello/weather/model/WeatherModel.java
@@ -7,7 +7,18 @@ import android.content.Context;
  * Created by zhushi on 2016/3/4.
  */
 public interface WeatherModel {
+
+    /**
+     * 加载天气数据
+     * @param cityName
+     * @param listener
+     */
     void loadWeatherData(String cityName, WeatherModelImpl.LoadWeatherListener listener);
 
+    /**
+     * 加载定位城市
+     * @param context
+     * @param listener
+     */
     void loadLocation(Context context, WeatherModelImpl.LoadLocationListener listener);
 }
